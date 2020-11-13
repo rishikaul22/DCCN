b02c6b8f27de966fc55f0f929255192595b0063b
Rishi Kaul <rishikaul@icloud.com>
2020-11-13 12:22:16 +0530

    NEW

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 8000))
 s.listen(5)
-print(socket.gethostname())
 
 while True:
 	clientsocket, address = s.accept()
